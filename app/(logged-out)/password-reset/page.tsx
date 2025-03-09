9748defea177e56a73d123ea7a5159a156a37140
liren <327381633@qq.com>
2025-03-09 18:22:06 +0800

    feat: finish the UI of reset password

--- app/(logged-out)/password-reset/page.tsx
+++ app/(logged-out)/password-reset/page.tsx
@@ -22,52 +22,40 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { passwordSchema } from "@/validation/passwordSchema";
-import { Button } from "@/components/ui/button";
-import { loginWithDentials } from "./actions";
-import { useRouter } from "next/navigation";
 
-const loginSchema = z.object({
+import { Button } from "@/components/ui/button";
+import { useSearchParams } from "next/navigation";
+
+const emailSchema = z.object({
   email: z.string().email({
     message: "Email is required!",
   }),
-  password: passwordSchema,
 });
 
-function LoginPage() {
-  const router = useRouter();
-
-  const form = useForm<z.infer<typeof loginSchema>>({
-    resolver: zodResolver(loginSchema),
+function PasswordReset() {
+  const searchParams = useSearchParams();
+  const email = searchParams.get("email") ?? "";
+  const form = useForm<z.infer<typeof emailSchema>>({
+    resolver: zodResolver(emailSchema),
     defaultValues: {
-      email: "",
-      password: "",
+      email: email,
     },
   });
 
-  const handleSubmit = async (data: z.infer<typeof loginSchema>) => {
+  const handleSubmit = async (data: z.infer<typeof emailSchema>) => {
     // clear errors before submit is a best practice
     form.clearErrors();
-
-    const response = await loginWithDentials({
-      email: data.email,
-      password: data.password,
-    });
-    if (response?.error) {
-      form.setError("root", {
-        message: response.message,
-      });
-    } else {
-      router.push("my-account");
-    }
+    console.log(data);
   };
 
   return (
     <main className="min-h-screen flex justify-center items-center">
       <Card className="w-[350px]">
         <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>Please login to your account</CardDescription>
+          <CardTitle>Password Reset</CardTitle>
+          <CardDescription>
+            Enter your email address to reset your password
+          </CardDescription>
         </CardHeader>
         <CardContent>
           <Form {...form}>
@@ -84,6 +72,7 @@ function LoginPage() {
                     <FormControl>
                       <Input
                         placeholder="John@Doe.com"
+                        defaultValue={email}
                         {...field}
                         type="text"
                       />
@@ -92,40 +81,24 @@ function LoginPage() {
                   </FormItem>
                 )}
               />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input placeholder="******" {...field} type="password" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
               {!!form.formState.errors.root?.message && (
                 <FormMessage>{form.formState.errors.root.message}</FormMessage>
               )}
-              <Button type="submit">Login</Button>
+              <Button type="submit">Submit</Button>
             </form>
           </Form>
         </CardContent>
         <CardFooter className="flex-col gap-2">
           <div className="text-muted-foreground text-sm">
-            Doesn&apos;t have an account?{" "}
-            <Link href="/register" className="underline">
-              Register
+            Remember your password?{" "}
+            <Link href="/login" className="underline">
+              Login
             </Link>
           </div>
           <div className="text-muted-foreground text-sm">
-            Forget password?{" "}
-            <Link
-              href={`/password-reset?email=${form.getValues("email")}`}
-              className="underline"
-            >
-              Reset my password
+            Don&apos;t have an account?{" "}
+            <Link href="/register" className="underline">
+              Register
             </Link>
           </div>
         </CardFooter>
@@ -134,4 +107,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default PasswordReset;
